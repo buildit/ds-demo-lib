371f816b9d7532615f28a9b53dfea4394cb62861
James Nash <james.nash@wipro.com>
2017-02-20 15:04:54 +0000

    Added poor man's grid system

--- src/header/index.jsx
+++ src/header/index.jsx
@@ -5,8 +5,12 @@ import logo from '../assets/unicorn_inc.png';
 const Header = (props) => {
   return (
     <header className="ds-demo-header">
-      <img src={logo} alt="Unicorn Inc"/>
-      <h1>{props.title}</h1>
+      <div>
+        <div className="logo">
+          <img src={logo} alt="Unicorn Inc"/>
+        </div>
+        <h1>{props.title}</h1>
+      </div>
 
       <nav>
         <ul>
@@ -16,6 +20,7 @@ const Header = (props) => {
           <li><a href="#" className="btn">Contact Us</a></li>
         </ul>
       </nav>
+
     </header>
   )
 };
