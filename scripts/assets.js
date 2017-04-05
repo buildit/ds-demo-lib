1d9d16aa65eacc1453b5e6dd43a4471904c62861
Panos Voudouris <panosvoudouris@gmail.com>
2017-04-05 09:41:10 +0100

    Move downloaded assets to separate dir then copy

--- scripts/assets.js
+++ scripts/assets.js
@@ -6,34 +6,41 @@ const frontify = require('../brandai.config').default.frontify;
 const chalk = require('chalk');
 const log = console.log;
 
-const assetsDir = 'src/assets';
-const brandaiDir = `${assetsDir}`;
+const assetsDir = './downloads';
+const brandaiDir = `${assetsDir}/brandai`;
 const frontifyDir = `${assetsDir}/frontify`;
+const targetDir = 'src/assets';
 
 // Brandai download
 // The logos
 log(`${chalk.white('Downloading logos from ')} ${chalk.blue(brandai.logos)}`);
-download(brandai.logos)
-  .then(data => {
-    const out = `${brandaiDir}/logo.zip`
-    fs.writeFileSync(out, data);
-    extract(out, {dir: brandaiDir}, (err) => {
-      if (err) {
-        log(chalk.red.dim('Error downloading logos '), chalk.red(err));
-      } else {
-        log(chalk.green.dim('Logo unzip done'));
-      }
-      fs.unlinkSync(out);
-    });
-    log(chalk.green.dim('Done downloading logos'));
+download(brandai.logos).then(data => {
+  const out = `${brandaiDir}/logo.zip`
+  fs.writeFileSync(out, data);
+  extract(out, {dir: brandaiDir}, (err) => {
+    if (err) {
+      log(chalk.red.dim('Error downloading logos '), chalk.red(err));
+    } else {
+      log(chalk.green.dim('Logo unzip done'));
+    }
+    fs.unlinkSync(out);
   });
+  log(chalk.green.dim('Done downloading logos'));
+})
+.then(() => {
+  log(chalk.white('Copying brand.ai assets'));
+  fs.createReadStream(`${brandaiDir}/styles.less`).pipe(fs.createWriteStream(`${targetDir}/styles.less`));
+});
 
-download(brandai.styles)
-  .then(data => {
-    const out = `${brandaiDir}/styles.less`
-    fs.writeFileSync(out, data);
-    log(chalk.green.dim('Done downloading styles'));
-  });
+download(brandai.styles).then(data => {
+  const out = `${brandaiDir}/styles.less`
+  fs.writeFileSync(out, data);
+  log(chalk.green.dim('Done downloading styles'));
+})
+.then(() => {
+  log(chalk.white('Copying brand.ai styles'));
+  fs.createReadStream(`${brandaiDir}/unicorn_inc.png`).pipe(fs.createWriteStream(`${targetDir}/unicorn_inc.png`));
+});
 
 // Frontify download
 log(`${chalk.white('Downloading Frontify devkit from ')} ${chalk.blue(frontify.devkit)}`);
@@ -49,10 +56,16 @@ download(frontify.devkit).then(data => {
     fs.unlinkSync(out);
   });
   log(chalk.green.dim('Done downloading Frontify devkit'));
+})
+.then(() => {
+  log(chalk.white('Copying Frontify devkit assets'));
 });
 
 log(`${chalk.white('Downloading Frontify logo from ')} ${chalk.blue(frontify.logo)}`);
 download(frontify.logo).then(data => {
   fs.writeFileSync(`${frontifyDir}/unicorn-logo.png`, data);
   log(chalk.green.dim('Done downloading Frontify logo'));
+})
+.then(() => {
+  log(chalk.white('Copying Frontify logo'));
 });
